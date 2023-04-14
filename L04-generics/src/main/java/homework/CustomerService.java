c9a01d7e4e7374fd62ec740aa4c263694be4aab5
alex1 <alex1706.mephi@gmail.com>
2023-04-14 15:39:09 +0300

    almost Done test scoreSortingTest

--- L04-generics/src/main/java/homework/CustomerService.java
+++ L04-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,30 @@
 package homework;
 
 
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CustomerService {
 
+    TreeMap<Customer, String> cs;
+
+    public CustomerService() {
+        cs = new TreeMap<>(Comparator.comparingLong(o -> o.getScores()));
+    }
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        return cs.firstEntry(); // это "заглушка, чтобы скомилировать"
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        return cs.higherEntry(customer); // это "заглушка, чтобы скомилировать"
     }
 
     public void add(Customer customer, String data) {
-
+        cs.put(customer, data);
     }
 }

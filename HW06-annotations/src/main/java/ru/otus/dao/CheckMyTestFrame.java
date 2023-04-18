df4ce7c6c97ff98f8a2636b34af9de123ce29928
alex1 <alex1706.mephi@gmail.com>
2023-04-18 16:32:52 +0300

    add annotation, my frame, my class to check my frame

---
+++ HW06-annotations/src/main/java/ru/otus/dao/CheckMyTestFrame.java
@@ -0,0 +1,16 @@
+package main.java.ru.otus.dao;
+
+import main.java.ru.otus.annotations.Test;
+
+public class CheckMyTestFrame {
+    @Test
+    public void testExample1() {
+        System.out.println("TEST 1");
+    }
+
+    @Test
+    public void testExample2() {
+        System.out.println("TEST 2");
+    }
+
+}

47de9ffa8f90623d78e7a2a5f6c00a6cf7aa3889
alex1 <alex1706.mephi@gmail.com>
2023-04-20 19:06:26 +0300

    Init 8 gc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,10 @@ include ("L02-gradle2-libApi")
 include ("L02-gradle2-libApiUse")
 include ("L03-qa")
 include ("L04-generics")
+include ("L05-collections")
+include ("L06-annotations")
+include("L08-gc:demo")
+include("L08-gc:homework")
 
 
 pluginManagement {

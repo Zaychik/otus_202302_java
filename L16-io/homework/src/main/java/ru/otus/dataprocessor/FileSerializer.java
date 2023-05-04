992ba76cacd07ada3ad69c56a4561ce5758af4f8
alex1 <alex1706.mephi@gmail.com>
2023-05-04 11:24:12 +0300

    I done hw using  Gson

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,25 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
+    String fileName;
 
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
-    public void serialize(Map<String, Double> data) {
+    public void serialize(Map<String, Double> data) throws IOException {
         //формирует результирующий json и сохраняет его в файл
-    }
+        try (Writer writer = new FileWriter(fileName)) {
+            Gson gson = new Gson();
+            gson.toJson(data, writer);
+        }
+     }
 }
--- L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,30 @@
 package ru.otus.dataprocessor;
 
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import ru.otus.model.Measurement;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
-
+    private String fileName;
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
-    public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+    public List<Measurement> load() throws IOException {
+        List<Measurement> list;
+        var gson = new Gson();
+        try (JsonReader jsonReader = new JsonReader(new InputStreamReader(ResourcesFileLoader.class.getClassLoader().getResourceAsStream("inputData.json")))) {
+            Type listType = new TypeToken<List<Measurement>>() {}.getType();
+            list = gson.fromJson(jsonReader, listType);
+        }
+        return list;
     }
 }

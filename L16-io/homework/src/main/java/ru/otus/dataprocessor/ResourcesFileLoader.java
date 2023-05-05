bfdb8a2b4af4a0a6ccbfe26ceef6384201aec5a3
alex1 <alex1706.mephi@gmail.com>
2023-05-05 16:46:58 +0300

    I done hw using  Jakson

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,28 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.otus.model.Measurement;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
+    private final ObjectMapper mapper = new ObjectMapper();
+    private String fileName;
 
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
-    public List<Measurement> load() {
+    public List<Measurement> load() throws IOException {
         //читает файл, парсит и возвращает результат
-        return null;
+        List<Measurement> myObjects = mapper.readValue(
+                new InputStreamReader(ResourcesFileLoader.class.getClassLoader().getResourceAsStream("inputData.json")),
+                new TypeReference<List<Measurement>>(){});
+        System.out.println(myObjects);
+        return myObjects;
     }
 }
